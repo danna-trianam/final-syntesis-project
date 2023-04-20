43608a1e863ef290386ef0a3f8a07c8dccefd307
danna-trianam <123768266+danna-trianam@users.noreply.github.com>
2023-04-20 12:06:16 -0500

    12:nxcjnsjcnjsnjns j

--- js/regression-analysis2.js
+++ js/regression-analysis2.js
@@ -39,12 +39,12 @@ function linearRegression(values_x, values_y) {
 
 //For kWh vs Month
 const months = [1, 2, 3, 4, 5];
-const kwhs = [240, 250, 274, 250, 300];
+const kwhs = [64, 67, 70, 62, 69];
 var l = linearRegression(months, kwhs);
 
 //For Residents vs kWh
-const residents = [1, 2, 3, 5, 5, 6, 3, 8, 3, 5];
-const kWhs = [240, 250, 274, 260, 210, 300, 260, 440, 300, 350];
+const residents = [3, 8, 5, 4, 4, 4, 4, 4, 3, 6];
+const kWhs = [98, 728, 260, 128, 205, 277, 159, 134, 110, 93];
 var r = linearRegression(residents, kWhs);
 
 const trace11 = {
